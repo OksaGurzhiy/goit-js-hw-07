edeb38ded9a35ac679a769c5c7f3fded8e163c4d
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-05-06 21:38:07 +0200

    #1task

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+const listElement = document.querySelector(".gallery");
+
+const imagesMarkup = galleryItems
+  .map(
+    ({ preview, original }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img  class="gallery__image" src="${preview}" data-source="${original}" alt="Image description"/></a></li>`
+  )
+  .join("");
+
+listElement.insertAdjacentHTML("afterbegin", imagesMarkup);
+
+listElement.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="800" height="600">
+`);
+
+  instance.show();
+  document.addEventListener("keydown", closeByEsc);
+
+  function closeByEsc(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeByEsc);
+    }
+  }
+});
