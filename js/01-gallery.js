d95ed5cb768555394e5d2f2a4ff3c93ed26164ce
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-05-06 23:25:21 +0200

    task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,17 +19,24 @@ listElement.addEventListener("click", (e) => {
   if (e.target.nodeName !== "IMG") {
     return;
   }
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src=${e.target.dataset.source} width="800" height="600">
-`);
+`,
+    {
+      onClose: () => {
+        document.removeEventListener("keydown", closeByEsc);
+      },
+    }
+  );
 
   instance.show();
+
   document.addEventListener("keydown", closeByEsc);
 
   function closeByEsc(e) {
     if (e.code === "Escape") {
       instance.close();
-      document.removeEventListener("keydown", closeByEsc);
     }
   }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listElement = document.querySelector(".gallery");
+
+const imagesMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img  class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+  )
+  .join("");
+
+listElement.insertAdjacentHTML("afterbegin", imagesMarkup);
+
+const options = {
+  captionPosition: "bottom",
+  captionDelay: 250,
+  captionsData: "alt",
+};
+
+const lightbox = new SimpleLightbox(".gallery a", options);
